8dd778e8b797d48a02d7aafd70da5ff3741140bf
Henrique <rick.eihara@gmail.com>
2019-05-12 04:55:15 -0300

    Refact: Encapsulate routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
-get '/live' do
-  HealthCheckController.new(self).status
-end
+class Routes < Sinatra::Base
+  get '/live' do
+    HealthCheckController.new(self).status
+  end
 
-post '/users' do
-  UserController.new(self).create
+  post '/users' do
+    UserController.new(self).create
+  end
 end
