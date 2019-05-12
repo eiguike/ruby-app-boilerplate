6f51e9e9fc67988a81f9b67d03a7a4571c50505f
Henrique <rick.eihara@gmail.com>
2019-05-12 18:18:55 -0300

    Feat: Treatments for common exceptions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,22 @@
 class Routes < Sinatra::Base
+  before do
+    content_type 'application/json'
+  end
+
+  error do |err|
+    begin
+      raise err
+    rescue ActiveRecord::RecordNotUnique => exception
+      status 202
+      return {
+        "msg": "Already exists this resource"
+      }.to_json
+    rescue BadRequestException => exception
+      status 400
+      exception.to_json
+    end
+  end
+
   get '/live' do
     HealthCheckController.new(self).status
   end
