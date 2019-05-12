6bb8700a57521425868e4eb57f58e9154db0a346
Henrique <rick.eihara@gmail.com>
2019-05-12 04:54:22 -0300

    Feat: Add database connection, create new user model

--- main.rb
+++ main.rb
@@ -1,4 +1,15 @@
 require 'sinatra' 
+require 'pry'
+require 'active_record'
 
+require './app/models/user'
+require './app/controllers/application_controller'
+require './app/controllers/user_controller'
 require './app/controllers/health_check_controller'
 require './config/routes'
+
+ActiveRecord::Base.establish_connection(
+  YAML.load_file("db/config.yml")[ENV["APP_ENV"]]
+)
+
+Routes.run!
