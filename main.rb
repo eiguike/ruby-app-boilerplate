af12d3ba8a35e707e4b8008876e8c5d4e5c41d5f
Henrique <rick.eihara@gmail.com>
2019-05-12 06:07:08 -0300

    Refact: Using relative_all gem

--- main.rb
+++ main.rb
@@ -2,11 +2,8 @@ require 'sinatra'
 require 'pry'
 require 'active_record'
 
-require './app/models/user'
-require './app/controllers/application_controller'
-require './app/controllers/user_controller'
-require './app/controllers/health_check_controller'
-require './config/routes'
+require_all './app'
+require_all './config'
 
 ActiveRecord::Base.establish_connection(
   YAML.load_file("db/config.yml")[ENV["APP_ENV"]]
