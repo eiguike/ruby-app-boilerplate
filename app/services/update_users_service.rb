f4e2ad49a30912cf124ee64215bac20589d933e6
Henrique <rick.eihara@gmail.com>
2019-05-12 23:07:17 -0300

    Feat: Add update users service

---
+++ app/services/update_users_service.rb
@@ -0,0 +1,32 @@
+class UpdateUsersService
+  def self.perform(args={})
+    new(args).perform
+  end
+
+  def initialize(args)
+    @user_entity = args[:user_entity] || User
+    @params = args[:params]
+  end
+
+  def perform
+    find_user
+    set_user_information
+    update_user 
+
+    return @user
+  end
+
+  private
+  def find_user
+    @user = @user_entity.find_by(login: @params[:login])
+    raise NotFoundException.new(:login) if @user.nil?
+  end
+
+  def set_user_information
+    @user.password = @params[:password]
+  end
+
+  def update_user
+    @user.save!
+  end
+end
