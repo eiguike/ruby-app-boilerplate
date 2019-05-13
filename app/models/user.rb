01944c7ac1ba7df99c693dc14cd6c49e83dbcdd2
Henrique <rick.eihara@gmail.com>
2019-05-12 22:39:54 -0300

    Feat: Encrypt password for new users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
+  validate :encrypt_password, on: [:create, :update]
 
+  def encrypt_password
+    self.password = BCrypt::Password.create(self.password)
+  end
 end
