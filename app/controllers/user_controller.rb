271bf0bb237578865e9b9a7a9a8fa5edb1f056be
Henrique <rick.eihara@gmail.com>
2019-05-12 23:08:03 -0300

    Feat: Add update, delete for users controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -10,6 +10,18 @@ class UserController < ApplicationController
     present(payload: user.extend(Presenters::User).to_json, status: 200)
   end
 
+  def delete 
+    set_params do |options|
+      param login: { type: String }
+    end
+
+    user = User.find_by(login: params[:login])
+    raise NotFoundException.new(:login) if user.nil?
+    user.delete
+
+    present(payload: {"result": "ok"}.to_json, status: 200)
+  end
+
   def create
     set_params do |options|
       param login: { type: String }
@@ -19,5 +31,16 @@ class UserController < ApplicationController
     user = CreateUsersService.perform(user_entity: User, params: params)
 
     present(payload: user.extend(Presenters::User).to_json, status: 201)
+  end
+
+  def update
+    set_params do |options|
+      param login: { type: String }
+      param password: { type: String }
+    end
+
+    user = UpdateUsersService.perform(user_entity: User, params: params)
+
+    present(payload: user.extend(Presenters::User).to_json, status: 200)
   end
 end
