858a1b739b644f634311a8b9deb40a8cd73031eb
Henrique <rick.eihara@gmail.com>
2019-05-12 22:42:11 -0300

    Feat: Create new method for user controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,15 @@
 class UserController < ApplicationController
+  def show
+    set_params do |options|
+      param login: { type: String }
+    end
+
+    user = User.find_by(login: params[:login])
+    raise NotFoundException.new(:login) if user.nil?
+
+    present(payload: user.extend(Presenters::User).to_json, status: 200)
+  end
+
   def create
     set_params do |options|
       param login: { type: String }
