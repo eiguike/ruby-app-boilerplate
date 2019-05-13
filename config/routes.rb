f4de9df712f0aad8754e1eebde02150ce21dd2be
Henrique <rick.eihara@gmail.com>
2019-05-12 22:41:12 -0300

    Feat: Add new routes for user controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ class Routes < ApplicationRoutes
     HealthCheckController.new(self).status
   end
 
-  post '/users' do
-    UserController.new(self).create
-  end
+  post '/users' do UserController.new(self).create end
+  get '/users/:login' do UserController.new(self).show end
+  patch '/users/:login' do UserController.new(self).update end
+  delete '/users/:login' do UserController.new(self).delete end
 end
