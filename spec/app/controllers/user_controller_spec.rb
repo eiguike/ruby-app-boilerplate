c879c5ca38a128b4a5444a731309b5676fe48d22
Henrique <rick.eihara@gmail.com>
2019-05-14 00:57:01 -0300

    Test: Organize specs

--- spec/app/controllers/user_controller_spec.rb
+++ spec/app/controllers/user_controller_spec.rb
@@ -1,61 +1,69 @@
-require 'spec_helper'
-
 describe UserController do
   include Rack::Test::Methods
 
-  let(:response) { post '/users', json, {'CONTENT_TYPE' => 'application/json'} }
-  let(:body) { JSON.parse(response.body) }
+  context "POST /users" do
+    let(:response) { post '/users', json, {'CONTENT_TYPE' => 'application/json'} }
+    let(:body) { JSON.parse(response.body) }
 
-  context "bad requests response without its two mandatory nodes" do
-    let(:json){ {}.to_json }
+    context "bad requests response without its two mandatory nodes" do
+      let(:json){ {}.to_json }
 
-    before do
-      response
+      before do
+        response
+      end
+
+      it { expect(body["error"]).to eq("The following fields (login, password) are missing!") }
+      it { expect(body["field"].length).to eq(2) }
+      it { expect(body["field"].first).to eq("login") }
+      it { expect(body["field"].second).to eq("password") }
+      it { expect(response.status).to eq(400) }
     end
 
-    it { expect(body["error"]).to eq("The following fields (login, password) are missing!") }
-    it { expect(body["field"].length).to eq(2) }
-    it { expect(body["field"].first).to eq("login") }
-    it { expect(body["field"].second).to eq("password") }
-    it { expect(response.status).to eq(400) }
-  end
+    context "bad requests response without login" do
+      let(:json){ { "password": "testpassword" }.to_json }
 
-  context "bad requests response without login" do
-    let(:json){ { "password": "testpassword" }.to_json }
+      before do
+        response
+      end
 
-    before do
-      response
+      it { expect(body["error"]).to eq("The following fields (login) are missing!") }
+      it { expect(body["field"].length).to eq(1) }
+      it { expect(body["field"].first).to eq("login") }
+      it { expect(response.status).to eq(400) }
     end
 
-    it { expect(body["error"]).to eq("The following fields (login) are missing!") }
-    it { expect(body["field"].length).to eq(1) }
-    it { expect(body["field"].first).to eq("login") }
-    it { expect(response.status).to eq(400) }
-  end
+    context "bad requests response without login" do
+      let(:json){ { "login": "somelogin@huaheuae.com" }.to_json }
 
-  context "bad requests response without login" do
-    let(:json){ { "login": "somelogin@huaheuae.com" }.to_json }
+      before do
+        response
+      end
 
-    before do
-      response
+      it { expect(body["error"]).to eq("The following fields (password) are missing!") }
+      it { expect(body["field"].length).to eq(1) }
+      it { expect(body["field"].first).to eq("password") }
+      it { expect(response.status).to eq(400) }
     end
 
-    it { expect(body["error"]).to eq("The following fields (password) are missing!") }
-    it { expect(body["field"].length).to eq(1) }
-    it { expect(body["field"].first).to eq("password") }
-    it { expect(response.status).to eq(400) }
-  end
+    context "success to create an user" do
+      let(:json){ { "login": "test@test.com", "password": "passwordtest" }.to_json }
+      let(:open_struct) { OpenStruct.new(:login => "test@test.com", :password => "passwordtest") }
 
-  context "success to create an user" do
-    let(:json){ { "login": "test@test.com", "password": "passwordtest" }.to_json }
-    let(:open_struct) { OpenStruct.new(:login => "test@test.com", :password => "passwordtest") }
+      before do
+        allow(CreateUsersService).to receive(:perform).and_return open_struct
+        response
+      end
 
-    before do
-      allow(CreateUsersService).to receive(:perform).and_return open_struct
-      response
+      it { expect(body["login"]).to eq("test@test.com") }
     end
-
-    it { expect(body["login"]).to eq("test@test.com") }
   end
 
+  context "GET /users/:login" do
+  end
+
+  context "PATCH /users/:login" do
+  end
+
+  context "DELETE /users/:login" do
+  end
 end
